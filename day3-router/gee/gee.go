eb16d3f945b8e9aaf0500973e718f6c969900a99
Thor S <tao.song@hex-tech.net>
2022-08-29 21:46:09 +0800

    有点迷糊

---
+++ day3-router/gee/gee.go
@@ -0,0 +1,32 @@
+package gee
+
+import "net/http"
+
+// HandlerFunc defines the request handler used by gee
+type HandlerFunc func(*Context)
+
+//Engine implement the interface of ServeHTTP
+type Engine struct {
+	router *router
+}
+
+func New() *Engine {
+	return &Engine{router: newRouter()} //New Engine  的时候也new 了route
+}
+
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	engine.router.addRoute(method, pattern, handler)
+}
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
+}
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	c := newContext(w, req)
+	engine.router.handler(c)
+}
